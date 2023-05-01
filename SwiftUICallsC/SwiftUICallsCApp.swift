44d6e3bfb7210ddaf7fea3494d5a6b0d11c7371d
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-05-01 10:44:13 -0500

    Initial Commit

---
+++ SwiftUICallsC/SwiftUICallsCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICallsCApp.swift
+//  SwiftUICallsC
+//
+//  Created by Mark Volkmann on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICallsCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
